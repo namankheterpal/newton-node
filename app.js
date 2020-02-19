e95f24771d469774f45b1497f8d51f846288c929
Naman Kheterpal <kheterpa@adobe.com>
2020-02-19 22:32:17 +0530

    class1,2

--- app.js
+++ app.js
@@ -1,4 +1,106 @@
 var express = require('express');
-var routes = express.Router();
+var path = require('path');
+var bodyParser = require('body-parser');
+var bcrypt = require('bcrypt');
+var jwt = require('jsonwebtoken');
 
-console.log('listning on 6000');
+var app = express();
+
+var users= [];
+
+var jsonParser = bodyParser.json();
+
+var mock = {
+    a:10
+}
+
+var privateKey ='abc';
+
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.get('/users',(req,res)=>{
+    res.json(users);
+})
+
+app.post('/verify',jsonParser,(req,res)=>{
+    let {token} = req.body;
+    try {
+        var decoded = jwt.verify(token, privateKey);
+        console.log(decoded);
+        res.send(decoded);
+      } catch(err) {
+          console.log('erros',err)
+      }
+})
+
+app.post('/login',jsonParser,(req,res)=>{
+    let  {email, password} = req.body;
+    let user = users.find(user => user.email===email);
+
+    console.log(user);
+
+    if(user){
+        bcrypt.compare(password, user.hash, function(err, result) {
+            if(err){
+                console.log(err);
+                res.send('error');
+            }
+            if(result){
+                // res.send('Logged in');
+                
+                // jwt.sign({ email, dob:'16Aug94' }, privateKey, { algorithm: 'RS256' }, function(err, token) {
+                //     if(err)console.log(err);
+                //     res.json({success:'Logged in', token});
+                // });
+
+                var token  = jwt.sign(
+                    { email, dob:'16Aug94' }, 
+                    privateKey,
+                    { expiresIn: '1h' }
+                    );
+                    
+                res.json({success:'Logged in', token});
+
+
+            } else{
+                res.send('Wrong password');
+            }
+        });
+    } else{
+        res.send('No User exist');
+    }
+
+})
+
+app.post('/signup',jsonParser,(req,res)=>{
+    let  {email, password} = req.body;
+
+   
+    
+    bcrypt.genSalt(10, function(err, salt) {
+        if(err){
+            console.log(err);
+        }
+        if(salt){
+            bcrypt.hash(password, salt , function(err, hash) {
+                if(err){
+                    console.log(err);
+                }
+                if(hash){
+                    users.push({
+                        email,
+                        hash
+                    });
+
+                    res.json({status: 'success'});
+                }
+            });
+        }
+    });
+    
+})
+
+app.listen(5001);
+
+
---
+++ temp.js
@@ -0,0 +1,7 @@
+var s = require('./app.js')
+
+function logger(name){
+    console.log(name);
+}
+
+module.exports = logger;
